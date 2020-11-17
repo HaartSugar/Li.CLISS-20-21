c12984b9d6de3221ac8d2fa70d4d23d18870e34f
HaartSugar <lishaopeng2003@gmail.com>
2020-11-17 17:03:44 +0100

    For Loop

--- CreateBoxes.js
+++ CreateBoxes.js
@@ -1,22 +1,24 @@
 var count = 0;
 var currentBoxcolor = "boxColor1";
-function CreateBoxes()
-{
-    count++
-    let addBox = '<div class="box '+currentBoxcolor + '">'+ count + '</div>';
-    if (currentBoxcolor == "boxColor1") 
-    {
-        currentBoxcolor = "boxColor2";
+function CreateBoxes() {
+    let str = document.getElementById("Times").value;
+    let numberofboxes = Number(str);
+    for (let i = 0; i < numberofboxes; i++) {
+
+
+        count++
+        let addBox = '<div class="box ' + currentBoxcolor + '">' + count + '</div>';
+        if (currentBoxcolor == "boxColor1") {
+            currentBoxcolor = "boxColor2";
+        }
+        else
+            if (currentBoxcolor == "boxColor2") {
+                currentBoxcolor = "boxColor3";
+            }
+            else
+                if (currentBoxcolor == "boxColor3") {
+                    currentBoxcolor = "boxColor1";
+                }
+        document.getElementById("Boxes").innerHTML += addBox;
     }
-    else 
-    if (currentBoxcolor == "boxColor2") 
-    {
-        currentBoxcolor = "boxColor3";
-    }
-    else
-    if (currentBoxcolor == "boxColor3") 
-    {
-        currentBoxcolor = "boxColor1";
-    }
-    document.getElementById("CreateBoxes").innerHTML += addBox;
 }
